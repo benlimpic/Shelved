b4aa7d2bc1429bc9b44fe6c4d35e723613526f34
init.defaultBranch <benlimpic@gmail.com>
2024-03-15 00:45:49 -0700

    clean

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header className="App-header">
+        <h1>Hello World!</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
