8a17cfe0616c2a7c65e8b76e518e9d9fc2e6046f
init.defaultBranch <benlimpic@gmail.com>
2024-03-15 04:25:00 -0700

    material ui test

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import "./App.css";
+import ButtonUsage from "./button";
+import Button from "@mui/material/Button";
+import * as React from "react";
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
+  const countZero = (event) => {
+    event.preventDefault();
+    setCount(0);
+  }
+
   return (
     <>
       <header className="App-header">
-        <h1>Hello World!</h1>
-        <h4>My name is Ben</h4>
-        <p>Let's be friends!</p>
+        <h1>Hello World</h1>
+        <h2>Count: {count}</h2>
+
+        <Button onClick={countZero} variant="outlined" size="small" color="primary">
+          Reset
+        </Button>
+        <ButtonUsage count={count} setCount={setCount} />
       </header>
     </>
   );
---
+++ src/button.jsx
@@ -0,0 +1,54 @@
+/* eslint-disable react/no-unescaped-entities */
+import Button from "@mui/material/Button";
+import PropTypes from 'prop-types';
+
+export default function ButtonUsage({ count, setCount }) {
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    setCount(count + 1);
+  };
+
+  const clicker = () => {
+    if (count === 0) {
+      return (
+        <>
+          <h2 style={{ color: "#E3E3E3" }}>My Name Is Ben</h2>
+          <Button onClick={handleClick} variant="contained" color="primary">
+            Let's Be Friends!
+          </Button>
+        </>
+      );
+    } else if (count !== 0 && count % 2 === 0) {
+      return (
+        <>
+          <h2 style={{ color: "#D3302F" }}>We Aren't Friends Anymore!</h2>
+          <Button onClick={handleClick} variant="contained" color="primary">
+            Let's Be Friends Again!
+          </Button>
+        </>
+      );
+    } else if (count !== 0 && count % 2 !== 0) {
+      return (
+        <>
+          <h2 style={{ color: "#1776D2" }}>We're The Best of Friends</h2>
+          <Button onClick={handleClick} variant="contained" color="error">
+            Stop Being Friends!
+          </Button>
+        </>
+      );
+    }
+  };
+
+  return (
+    <>
+      {clicker()}
+    </>
+  )
+}
+
+ButtonUsage.propTypes = {
+  count: PropTypes.number.isRequired,
+  setCount: PropTypes.func.isRequired,
+};
+
