d6606fde43dcafb3f3f0b1659e7bbedf61be488d
init.defaultBranch <benlimpic@gmail.com>
2024-03-15 19:38:10 -0700

    css test

--- src/header.jsx
+++ src/header.jsx
@@ -13,26 +13,34 @@ export default function Header() {
     setCount(0);
   };
 
+  const fade = () => {
+    document.querySelector(".fader").classList.add("fade");
+    setTimeout(() => {
+      document.querySelector(".fader").classList.remove("fade");
+    }, 250);
+  }
+
   React.useEffect(() => {
     if (count === 0) {
-      // setLogoSpeed(0);
+      fade();
       setHeaderColor("#E3E3E3");
       setHeaderText("Hello World");
     } else if (count % 2 === 0) {
-      // setLogoSpeed(5);
+      fade();
       setHeaderColor("#D3302F");
       setHeaderText("BooHoo!");
     } else if (count % 2 !== 0) {
-      // setLogoSpeed(5);
+      fade();
       setHeaderColor("#1776D2");
       setHeaderText("WooHoo!");
     }
-  }, [count]);
+  }, [count, setCount]);
 
   const wobble = (content) => {
     let text = content.split("");
     const textCode = text.map((x, idx) => {
       let delay = (idx + 1) * 50;
+
       return (
         <span
           key={idx}
@@ -52,11 +60,11 @@ export default function Header() {
         <div className="wobble" style={{ color: `${headerColor}` }}>
           <>
             {count === 0 ? (
-              <div className="logo">
+              <div className="logo fader">
                 <h1>{headerText}</h1>
               </div>
             ) : (
-              <div className="logo">{wobble(headerText)}</div>
+              <div className="logo fader">{wobble(headerText)}</div>
             )}
           </>
         </div>
